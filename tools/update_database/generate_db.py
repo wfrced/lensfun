683bde4a5829d09674d1588a1cb1d8217499fe81
Artem Gorbatiuk <wfrced@gmail.com>
2024-12-18 21:49:53 +0100

    Nix-compatible timestamp for db updates

--- tools/update_database/generate_db.py
+++ tools/update_database/generate_db.py
@@ -1,7 +1,6 @@
 import argparse
 import glob
 import os
-import subprocess
 
 import xml_converter
 
@@ -12,12 +11,18 @@ if __name__ == "__main__":
                                                  "(tar balls for all Lensfun versions).")
     parser.add_argument("-i", "--input", default="data/db/", help="input folder containing XML files")
     parser.add_argument("-o", "--output", default="db/", help="output folder for generated db")
+    parser.add_argument("-t", "--timestamp", type=int, default=None, help="Timestamp to use for file modification times")
     args = parser.parse_args()
 
     xml_filenames = glob.glob(os.path.join(args.input, "*.xml"))
     xml_files = set(xml_converter.XMLFile(filename) for filename in xml_filenames)
-    timestamp = int(subprocess.check_output(["git", "log", "-1", "--format=%ad", "--date=raw", "--", "*.xml"],
-                                            cwd=args.input).decode("utf-8").split()[0])
+
+    if args.timestamp is not None:
+        timestamp = args.timestamp
+    else:
+        # Fallback to SOURCE_DATE_EPOCH for reproducible builds
+        timestamp = int(os.environ.get("SOURCE_DATE_EPOCH", "0"))
+
     print(timestamp)
 
     xml_converter.generate_database_tarballs(xml_files, timestamp, args.output)
